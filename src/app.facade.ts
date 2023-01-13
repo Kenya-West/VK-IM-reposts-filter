92c6b9474ee43f30ee794388de45fe738f6230bd
Innokenty Ivanov <Kenya-West@outlook.com>
2023-01-13 21:11:06 +0600

    Add actual method to render button

--- src/app.facade.ts
+++ src/app.facade.ts
@@ -4,23 +4,28 @@ import { ButtonIcons } from "./controls/buttons/button.model";
 import { ElementFind, GetElementCollection } from "./element-find/element-find";
 import { RenderAt } from "./render/render.fabric";
 import { ElementCollection } from "./element-find/element-collection";
+import { ThreeDotsButton } from "./controls/buttons/three-dots-button/three-dots-button";
 
-export function AddButtons() {
+export function addButtons() {
     // create button
-    const addButtonAddAll = new Button({
-            id: "sd-add-all",
-            classes: ["btn", "btn-default"],
-            icon: ButtonIcons.glyphiconPicture,
-            text: "Lorem ipsum"
+    const addButtonAddAll = new ThreeDotsButton({
+            id: "vk-im-resposts-filter-add-button-threedots",
+            tag: "a",
+            classes: ["ui_actions_menu_item", "im-action", "im-action_search", "_im_action"],
+            attributes: { "tabindex": "0", "role": "link"},
+            icon: ButtonIcons.none,
+            text: "Поиск репостов",
         },
         ConsoleLogAction.prototype.log,
         {}).element;
 
     // find place for button
-    const elementData = GetElementCollection.get(ElementCollection.Root);
+    const elementData = GetElementCollection.get(ElementCollection.IMDialogToolBarMoreActionsPanel)!;
+    const insertBeforeData = GetElementCollection.get(ElementCollection.IMDialogToolBarMoreActionsPanelSecondButton)!;
     if (elementData) {
         const element = new ElementFind().getSingle(elementData);
+        const insertBeforeElem = new ElementFind().getSingle(insertBeforeData);
         // render button
-        new RenderAt().render(addButtonAddAll, element);
+        new RenderAt().render(addButtonAddAll, element, insertBeforeElem);
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import { routeGuardExact } from "./guards/route.guard";
+import { routeGuardExact, routeGuardIncludes } from "./guards/route.guard";
 import { elementShouldNotExistGuard, elementShouldExistGuard } from "./guards/element-existence.guard";
 import { Routes } from "./routing/routes";
 import { GetElementCollection } from "./element-find/element-find";
 import { ElementCollection } from "./element-find/element-collection";
-import { AddButtons } from "./app.facade";
+import { addButtons } from "./app.facade";
 
 export class App {
     constructor() {
         console.log("Скрипт инициализирован!");
     }
 
-    @routeGuardExact(Routes.IM)
-    @elementShouldNotExistGuard("#sd-add-all-public")
-    @elementShouldExistGuard(GetElementCollection.get(ElementCollection.Root)?.selector)
+    @routeGuardIncludes(Routes.IMSelected)
+    @elementShouldNotExistGuard(GetElementCollection.get(ElementCollection.IMDialogToolBarMoreActionsPanelRepostsFilterButton)!.selector)
+    @elementShouldExistGuard(GetElementCollection.get(ElementCollection.IMDialogToolBarMoreActionsPanel)?.selector)
     public addButtons(): void {
         console.log("addButtons запущен!");
-        AddButtons();
+        addButtons();
     }
 }

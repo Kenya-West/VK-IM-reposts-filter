b58741f6ab2728a51df13da055a5252481a0495a
Innokenty Ivanov <Kenya-West@outlook.com>
2023-01-20 22:53:51 +0600

    Fix - use Counter control

--- src/actions/panel-set-state.action.ts
+++ src/actions/panel-set-state.action.ts
@@ -1,6 +1,7 @@
 import { ButtonFlat } from "../controls/buttons/button-flat/button-flat.control";
 import { ButtonIcons } from "../controls/buttons/button.model";
 import { ControlBase } from "../controls/control-base.control";
+import { Counter } from "../controls/counter/counter/counter.control";
 import { ElementCollection } from "../element-find/element-collection";
 import { GetElementCollection, ElementFind } from "../element-find/element-find";
 import { elementShouldExistGuard } from "../guards/element-existence.guard";
@@ -55,7 +56,7 @@ export class PanelSetStateAction implements ActionAbstract {
             new RenderAt().render(addSearchButton, element);
         }
         function renderCounter() {
-            const addCounter = new ButtonFlat({
+            const addCounter = new Counter({
                 id: "vk-im-resposts-counter",
                 tag: "div",
                 classes: ["addpost_button_wrap"],
@@ -63,11 +64,8 @@ export class PanelSetStateAction implements ActionAbstract {
                     key: "margin-left",
                     value: "20px"
                 }],
-                html: `Количество репостов: <span></span>`,
-                icon: ButtonIcons.none
-            },
-                ConsoleLogAction.prototype.log,
-                {}).element;
+                html: `Количество репостов: <span></span>`
+            }).element;
 
             const element = new ElementFind().getElementByElementIdSingle(ElementCollection.IMDialogContainerFilterPanelContainer);
             // render button

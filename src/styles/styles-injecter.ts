152934a542ccc90e14b8adda2284daab8493e363
Innokenty Ivanov <Kenya-West@outlook.com>
2023-01-28 20:35:00 +0600

    Update styles injecter

--- src/styles/styles-injecter.ts
+++ src/styles/styles-injecter.ts
@@ -1,14 +1,17 @@
 import style from "../assets/styles/styles.scss";
+import { checkUserscriptPermission } from "../guards/userscript-permissions.guard";
 
 export class StylesInjecter {
     // this is the style element that will be injected into the DOM at script"s start
     public injectInit() {
-        if (typeof GM_addStyle === "function") {
-            GM_addStyle(style);
-        } else {
-            console.error("GM_addStyle is not defined");
-        }
+        style;
     }
+
+    /**
+     * @deprecated Tampermonkey does not support this method since 2018
+     * @param css: a css string
+     */
+    @checkUserscriptPermission("GM_addStyle")
     public static inject(css: string) {
         GM_addStyle(css);
     }

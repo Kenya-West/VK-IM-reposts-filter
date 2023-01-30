1cd0f6127494d5143a578cb74144a4b835f60161
Innokenty Ivanov <Kenya-West@outlook.com>
2023-01-30 21:07:59 +0600

    Replace env .ts files with just env

--- src/environment/environment.development.ts
+++
@@ -1,5 +0,0 @@
-import { EnvironmentModel } from "./environment";
-
-export const Environment: EnvironmentModel = {
-    mode: "development",
-}
--- src/environment/environment.ts
+++
@@ -1,7 +0,0 @@
-export interface EnvironmentModel {
-    mode: "development" | "production";
-}
-
-export const Environment: EnvironmentModel = {
-    mode: "development"
-}
